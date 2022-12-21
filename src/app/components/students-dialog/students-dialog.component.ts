98f21f812ae9ecbd8c2eb4d40e5a76561227e19a
exe029 <exe029@hotmail.com>
2022-12-20 22:47:00 -0300

    pre-entrega.1

--- src/app/components/students-dialog/students-dialog.component.ts
+++ src/app/components/students-dialog/students-dialog.component.ts
@@ -1,9 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import { DataService } from 'src/app/service/data.service';
-import { Student } from '../../interface/students';
-
-
 
 @Component({
   selector: 'app-students-dialog',
@@ -14,8 +10,6 @@ export class StudentsDialogComponent implements OnInit {
 
   constructor( @Inject(MAT_DIALOG_DATA) public data: Element | null,) {
     console.log(data)
-    // if (data){
-    //}
    }
 
   ngOnInit(): void {
